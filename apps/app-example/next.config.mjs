d5b9f93ad024ca37a7339e3ee5e7ad4172422655
Ivan <iverzilli>
2025-04-07 11:10:06 +0200

    Aggiunta della configurazione i18n per la lingua italiana nel file di configurazione di Next.js, abilitando il supporto per la localizzazione.

--- apps/app-example/next.config.mjs
+++ apps/app-example/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['it'],
+    defaultLocale: 'it',
+    localeDetection: false,
+  },
   // Aggiungeremo configurazioni specifiche qui (es. i18n, output, etc.)
 };
 
